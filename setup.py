16c31b811b6a7f319981098659d0713f9c7d4f7a
xkrrmswn <xkrrmswn@kyowon.co.kr>
2021-05-07 14:07:18 +0900

    tkj_lib_ver2

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="tkj_lib",
+    version="0.0.1",
+    description="keunjoo pip install test",
+    url="https://github.com/keunjoo-tak/git_pip_install.git",
+    author="takkeunjoo",
+    author_email="xkrrmswn@gmail.com",
+    license="keunjoo",
+    packages=["tkj_lib"],
+    zip_safe=False,
+    install_requires=["numpy==1.18.3"],
+)
---
+++ tkj_lib/tkj.py
@@ -0,0 +1,2 @@
+def print_tkj(msg):
+    print(f"안녕하세요? 탁근주입니다. {msg}")
